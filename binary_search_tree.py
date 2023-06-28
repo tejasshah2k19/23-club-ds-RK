ce7f32866da665d9edec7025ec6001363295a835
tejas <tejasshah@gmail.com>
2023-06-28 14:02:21 +0530

    delete in bst

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -31,6 +31,36 @@ def preorder(root):  #root-left-right
 def postorder(root): #left-right-root 
     pass 
 
+def deleteNode(root,data): #200,175
+    if root is None: 
+        return root 
+    
+    if root.data == data :
+        #0 1 child -> left  
+        if root.left == None : 
+            tmp = root.right 
+            del root 
+            return tmp
+        elif root.right == None: 
+            tmp = root.left 
+            del root
+            return tmp 
+        else:
+            #2 child 
+            success = root.right 
+            while success.left != None:
+                success = success ->left 
+            root.data = success.data 
+            return deleteNode(root.right,success.data)
+        
+    elif root.data > data :
+        #left 
+        root.left = deleteNode(root.left,data)
+    elif root.data < data :
+        #right 
+        root.right = deleteNode(root.right,data)
+    
+
 
 root = None
 
@@ -38,7 +68,7 @@ root = None
 
 
 while True: 
-    print("\n0 For Exit\n1 For Add\n2 Print\nEnter choice")
+    print("\n0 For Exit\n1 For Add\n2 Print\n3 For Delete\nEnter choice")
     choice = int(input())
     if choice == 1 :
         
@@ -48,3 +78,6 @@ while True:
         inorder(root)
     elif choice ==  0:
         exit(0)
+    elif choice == 3:
+        data = int(input("Enter num that you want to delete"))
+        deleteNode(root,data)        
